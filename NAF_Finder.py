04576a92b149d5deec4f2f21dca2d5e7c9485616
mathisgentine <34328929+mathisgentine@users.noreply.github.com>
2021-02-03 18:35:16 +0100

    Update NAF_Finder.py

--- NAF_Finder.py
+++ NAF_Finder.py
@@ -19,6 +19,9 @@ def get_naf_code(siren_number):
     	info_dict["City"] = res["unite_legale"]["etablissement_siege"]["libelle_commune"]
     	info_dict["NAF"] = str(res["unite_legale"]["activite_principale"]).replace('.','')
     	info_dict["Creation_date"] = res["unite_legale"]["date_creation"]
+        info_dict["Legal_category"] = res["unite_legale"]["categorie_juridique"]
+        info_dict["TVA_Intra"] = res["unite_legale"]["numero_tva_intra"]
+        info_dict["Name"] = res["unite_legale"]["denomination"]
 
     	return info_dict
     except:
@@ -27,8 +30,11 @@ def get_naf_code(siren_number):
 SIREN_to_check = st.number_input('Insert SIREN number', value=804645034)
 res = get_naf_code(SIREN_to_check)
 
+st.write('**Legal name is :** ', res["Name"])
 st.write('**Address is :** ', res["Address"])
 st.write('**ZIP is :** ', res["ZIP"])
 st.write('**City is :** ', res["City"])
 st.write('**NAF is :** ', res["NAF"])
 st.write('**Date of creation is :** ', res["Creation_date"])
+st.write('**Legal cateogry is :** ', res["Legal_category"])
+st.write('**TVA Intracommunautaire is :** ', res["TVA_Intra"])
